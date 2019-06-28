8d843139ff2445c9111a562f20cc439fef20d4be
David <chengdavid2@gmail.com>
2019-06-27 22:57:34 -0700

    Added comments in NLP and OCR
    Added comments for functions in NLP and OCR

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -16,32 +16,33 @@ import numpy as np
 from PIL import Image, ImageEnhance, ImageFilter
 
 
+
 # INPUT: img_path - path to image that tesseract will process
 #        txt_path - path to text file that tesseract will write data to.
-# DESCRIPTION: Write ocr data to .csv file.
+# OUTPUT: None
+# DESCRIPTION: writes OCR data to a text file in form of tab delimited csv.
 def extract_text(img_path, txt_path):
-    # Returns text from image in form as string and then converts string into Pandas dataframe
     text = pytesseract.image_to_data(Image.open(img_path)) # Tesseract OCR
     # Write tab delimited string into txt file
     file = open(txt_path, 'w')
     file.write(text)
     file.close
 
-# INPUT: img_path - path to image that tesseract will process
-# OUTPUT: text - string of ocr data
-def extract_string(img_path):
-    # returns text from image in form as string
-    text = pytesseract.image_to_string(Image.open(img_path))  # Tesseract OCR)
-    return text
 
 # INPUT: text_path - path to csv data
-# OUTPUT: pandas dataframe created from csv file.
+# OUTPUT: pandas dataframe created from csv file
+# DESCRIPTION: read tab delimited csv text file of OCR data and returns as pandas dataframe.
 def text_to_dataframe(text_path):
-    # Extract txt file into pandas dataframe and returns dataframe
-    df = pd.read_csv(text_path, sep='\t',engine='python',quotechar="'"or'"')
-    df = df.dropna()  # drop rows with text as nan
-    #df.to_excel('tmp/ocr_data.xlsx') # Why do we need both? you can display csv with formatting.
-    return df
+    ocr_df = pd.read_csv(text_path, sep='\t',engine='python',quotechar="'"or'"')
+    ocr_df = ocr_df.dropna()  # drop rows with text as nan
+    return ocr_df
+
+# INPUT: img_path - path to image that tesseract will process
+# OUTPUT: text - string of ocr data
+# DESCRIPTION: returns text from image in form as string
+def extract_string(img_path):
+    text = pytesseract.image_to_string(Image.open(img_path))  # Tesseract OCR)
+    return text
 
 # INPUT: dataframe
 # OUTPUT: dataframe with all commas removed from the end of text.
@@ -81,6 +82,11 @@ def format_df(df):
 #         +-------+----------+-----------+---------+----------+----------+------+-----+-------+--------+------+------+  /
 #   N     |       |          |           |         |          |          |      |     |       |        |      |      | /
 #         +-------+----------+-----------+---------+----------+----------+------+-----+-------+--------+------+------+/
+
+
+# INPUT: names - list of potential names for patient
+#        formatted_df - formatted pandas dataframe of OCR data
+# OUTPUT: name_candidate_dict - dictonary - key = name : value = array of tesseract data
 # DESCRIPTION: This function creates a candidate table given the names list and formatted dataframe.
 def create_name_candidates(names, formatted_df):
     name_candidate_dict = {}
@@ -98,7 +104,7 @@ def create_name_candidates(names, formatted_df):
     return name_candidate_dict
 
 # INPUT: phns - list of phn strings
-#        df - master ocr dataframe
+#        formatted_df - formatted pandas dataframe of OCR data
 # OUTPUT: dictonary - key = name : value = array of tesseract data
 # DESCRIPTION: This function creates a candidate table given the phn list and formatted dataframe.
 def create_phn_candidates(phns, formatted_df):
@@ -109,10 +115,10 @@ def create_phn_candidates(phns, formatted_df):
 
     return phn_candidate_dict
 
-# INPUT: formatted_df dataframe of ocr output with comma striped.
+# INPUT: formatted_df - dataframe of ocr output with comma striped.
 #        name string - (first last)
-# OUTPUT: success flag if name is found in ocr data
-# DESCRIPTION:
+# OUTPUT: Boolean success flag if name is found in ocr data
+# DESCRIPTION: Look for name in formatted dataframe
 def look_for_name(formatted_df, name):
     print ('look_for_name debug - validating '+name)
     first_last = name.split(' ', 1)
